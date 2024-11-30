8cd5922c698ed14ec851ef9bb6f8f3c6d361cf79
Aмαrкo <152167635+Amar-dev-coder@users.noreply.github.com>
2024-11-30 17:31:07 +0100

    4 Add Admin class

---
+++ backend/RS1_2024_25.API/Data/Models/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RS1_2024_25.API.Data.Models
+{
+    public class Admin
+    {
+        public int AdminId { get; set; } 
+        public string Name { get; set; } 
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+
+        
+        public UserAccount UserAccount { get; set; }
+    }
+}
