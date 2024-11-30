b82cadbe7d21436381727d64f8f1e91af9fca4fe
Aмαrкo <152167635+Amar-dev-coder@users.noreply.github.com>
2024-11-30 17:32:49 +0100

    5 Add UserAccount class

---
+++ backend/RS1_2024_25.API/Data/Models/UserAccount.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RS1_2024_25.API.Data.Models
+{
+    public class UserAccount
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool isUser {  get; set; }
+        public bool is2FActive { get; set; }
+
+        [ForeignKey(nameof(Admin))]
+        public int? AdminId { get; set; }
+        public Admin? Admin { get; set; }
+
+        [ForeignKey(nameof(User))]
+
+        public int? UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
