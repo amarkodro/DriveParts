4a1b391e74c31ab0711dd4a133e6c8b4e324270b
Aмαrкo <152167635+Amar-dev-coder@users.noreply.github.com>
2024-12-01 21:44:44 +0100

    7 Add User class

---
+++ backend/RS1_2024_25.API/Data/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RS1_2024_25.API.Data.Models
+{
+    public class User : UserAccount
+    {
+        [ForeignKey(nameof(Gender))]
+        public int GenderId { get; set; }
+        public Gender Gender { get; set; }
+
+        [ForeignKey(nameof(City))]
+        public int CityId { get; set; }
+        public City City { get; set; } 
+    }
+}
