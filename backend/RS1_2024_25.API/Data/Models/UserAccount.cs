cd90eef72017c9ce70d70aece8d7ac6d3f5da3af
Aмαrкo <152167635+Amar-dev-coder@users.noreply.github.com>
2024-12-02 13:32:49 +0100

    9 Update class UserAccount

--- backend/RS1_2024_25.API/Data/Models/UserAccount.cs
+++ backend/RS1_2024_25.API/Data/Models/UserAccount.cs
@@ -5,19 +5,15 @@ namespace RS1_2024_25.API.Data.Models
     public class UserAccount
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public bool IsAdmin { get; set; }
         public bool isUser {  get; set; }
         public bool is2FActive { get; set; }
-
-        [ForeignKey(nameof(Admin))]
-        public int? AdminId { get; set; }
-        public Admin? Admin { get; set; }
-
-        [ForeignKey(nameof(User))]
-
-        public int? UserId { get; set; }
-        public User? User { get; set; }
     }
 }
