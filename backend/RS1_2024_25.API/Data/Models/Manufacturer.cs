cfda93db6d70c4e52e37d674bb9fc692afa6c5e8
Aмαrкo <152167635+Amar-dev-coder@users.noreply.github.com>
2024-12-03 17:06:17 +0100

    20 Add class Manufacturer

---
+++ backend/RS1_2024_25.API/Data/Models/Manufacturer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RS1_2024_25.API.Data.Models
+{
+    public class Manufacturer
+    {
+        [Key]
+        public int ManufacturerId { get; set; }
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public string Address { get; set; }
+
+      
+    }
+}
