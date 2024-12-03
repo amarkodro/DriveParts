01a23893d2efd10cf81613582e225ad1cfbe21b8
Aмαrкo <152167635+Amar-dev-coder@users.noreply.github.com>
2024-12-03 16:54:46 +0100

    16 Add class FAQ

---
+++ backend/RS1_2024_25.API/Data/Models/FAQ.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RS1_2024_25.API.Data.Models
+{
+    public class FAQ
+    {
+        public int FAQId { get; set; }
+
+        public string Question { get; set; }
+        public string  Answer { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        
+
+    }
+}
