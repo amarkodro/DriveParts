6baa41fac456e02ed90a4fb63f831349f3b363ee
Aмαrкo <152167635+Amar-dev-coder@users.noreply.github.com>
2024-12-03 17:00:40 +0100

    18 Add class Car

---
+++ backend/RS1_2024_25.API/Data/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RS1_2024_25.API.Data.Models
+{
+    public class Car
+    {
+
+        public int CarId { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public int Year { get; set; }
+
+        
+    }
+}
