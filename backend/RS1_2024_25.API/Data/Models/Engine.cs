b4805571650a419e3915b01c1627efc372d8eba7
Aмαrкo <152167635+Amar-dev-coder@users.noreply.github.com>
2024-12-03 16:57:59 +0100

    17 Add class Engine

---
+++ backend/RS1_2024_25.API/Data/Models/Engine.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RS1_2024_25.API.Data.Models
+{
+    public class Engine
+    {
+  
+        public int EngineId { get; set; }
+        public string Name { get; set; }
+        public int Power { get; set; }
+        public double Displacement { get; set; }
+        public string FuelType { get; set; }
+
+       
+    }
+}
