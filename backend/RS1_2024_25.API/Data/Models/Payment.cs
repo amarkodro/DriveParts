9c71f59a6f63470b6a622415da79bfc1c9c7e8e0
Aмαrкo <152167635+Amar-dev-coder@users.noreply.github.com>
2024-12-04 11:56:18 +0100

    28 Add class Payment

---
+++ backend/RS1_2024_25.API/Data/Models/Payment.cs
@@ -0,0 +1,9 @@
+﻿namespace RS1_2024_25.API.Data.Models
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public bool IsCard { get; set; }
+        public bool IsCash { get; set; }
+    }
+}
