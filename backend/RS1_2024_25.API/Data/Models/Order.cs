0263f30b363ab2e4cd853d9a63cfddafcf5fb5fb
Aмαrкo <152167635+Amar-dev-coder@users.noreply.github.com>
2024-12-04 11:52:23 +0100

    25 Add class Order

---
+++ backend/RS1_2024_25.API/Data/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RS1_2024_25.API.Data.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime Date { get; set; }
+
+        [ForeignKey(nameof(Status))]
+        public int StatusId { get; set; }
+        public Status Status { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        [ForeignKey(nameof(Supplier))]
+        public int SupplierId { get; set; }
+        public Supplier Supplier { get; set; }
+
+    }
+}
