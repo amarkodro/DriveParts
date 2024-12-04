abccc0a497ae40d906c0e019d01acd34fe380b04
Aмαrкo <152167635+Amar-dev-coder@users.noreply.github.com>
2024-12-04 11:53:56 +0100

    26 Add class OrderItem

---
+++ backend/RS1_2024_25.API/Data/Models/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RS1_2024_25.API.Data.Models
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+
+        [ForeignKey(nameof(Order))]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
+        [ForeignKey(nameof(Part))]
+        public int PartId { get; set; }
+        public Part Part { get; set; }
+
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+    }
+}
