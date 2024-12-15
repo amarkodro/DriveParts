1ed1ac73a8d83d7a53dc81c8b8f2bcca7e6bd485
CePuu1337 <ammarpuce2003@gmail.com>
2024-12-15 23:54:17 +0100

    49 Add CarPartsClass

---
+++ backend/RS1_2024_25.API/Data/Models/CarPart.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RS1_2024_25.API.Data.Models
+{
+    public class CarPart
+    {
+        public int CarPartId { get; set; }
+
+        [ForeignKey(nameof(Car))]
+        public int CarId { get; set; }
+        public Car Car { get; set; }
+
+        [ForeignKey(nameof(Part))]
+        public int PartId { get; set; }
+        public Part Part { get; set; }
+
+
+    }
+}
