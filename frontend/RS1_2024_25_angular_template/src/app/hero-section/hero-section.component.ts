9eae1865b28c4e26cd81ce3d4c1fa3f43bae9bf2
Aмαrкo <152167635+Amar-dev-coder@users.noreply.github.com>
2024-12-30 17:18:46 +0100

    76 Implementing a slide show in the hero-section

--- frontend/RS1_2024_25_angular_template/src/app/hero-section/hero-section.component.ts
+++ frontend/RS1_2024_25_angular_template/src/app/hero-section/hero-section.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero-section',
   templateUrl: './hero-section.component.html',
-  styleUrl: './hero-section.component.css'
+  styleUrls: ['./hero-section.component.css']
 })
-export class HeroSectionComponent {
+export class HeroSectionComponent implements OnInit {
+  images: string[] = [
+    '/assets/banner.jpg',
+    '/assets/banner2_valeo.jpg',
+    '/assets/continental_banner2.jpg'
+  ];
+  currentIndex: number = 0;
 
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 3000);
+  }
+
+  get currentImage(): string {
+    return this.images[this.currentIndex];
+  }
 }
